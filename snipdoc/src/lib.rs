bb7b7c4e2aaf58ed451c3a5cba89644243cb711a
Elad Kaplan <kaplan.elad@gmail.com>
2024-05-29 18:14:41 +0300

    update readme

--- snipdoc/src/lib.rs
+++ snipdoc/src/lib.rs
@@ -1,4 +1,11 @@
-//! # Snipdoc
+//! <div align="center">
+//!   <img src="https://github.com/kaplanelad/snipdoc/raw/main/media/logo.png>
+//!   <h1>Snipdoc</h1>
+//!   <h3>Managing documentation that includes code snippets</h3>
+//!
+//!   [![crate](https://img.shields.io/crates/v/snipdoc.svg)](https://crates.io/crates/snipdoc)
+//!   [![docs](https://docs.rs/snipdoc/badge.svg)](https://docs.rs/snipdoc)
+//! </div>
 //!
 //! **Snipdoc** is a straightforward tool for managing documentation that
 //! includes code snippets. It collects snippets from your code or YAML files
@@ -6,94 +13,109 @@
 //! documentation process more efficient.
 //!
 //! ## Installation
-//! #### Cargo install:
+//! To install Snipdoc, you can use Cargo:
 //! ```sh
 //! cargo install snipdoc
 //! ```
-//! Or download from the [GitHub Repository.](https://github.com/kaplanelad/snipdoc/releases/latest)
-//! ## Getting started:
 //!
-//! ### Collecting Snippets
-//! To collect snippets, follow these steps:
-//! 1. Identify the piece of content in your code that you want to grab.
-//! 2. Wrap the snippet with the following tag:
-//! ```text
-//! // <snip id="SNIPPET_ID">
-//!      CONTENT HERE
-//! // </snip>
-//! ```
-//! 3. You can hide the snippet from presentation using various comment styles:
-//!     - In Markdown files (like readme.md), use HTML comments: `<!-- CONTENT
-//!       HERE -->`
-//!     - In Rust file docs, use: `//! CONTENT HERE`
-//!     - In Rust function docs, use: `/// CONTENT HERE`
-//!     - Use appropriate comment tags for other file formats.
+//! Or download it from the [GitHub Repository.](https://github.com/kaplanelad/snipdoc/releases/latest)
 //!
-//! ### Injecting Snippets
-//! To inject snippets, create an empty placeholder with the snippet ID and add
-//! `inject_from="code"` attribute like this:
-//!
-//! ```text
-//! <!-- <snip id="SNIPPET_ID" inject_from="code"> -->
-//!      CONTENT HERE
-//! <!-- </snip> -->
-//! ```
-//!
-//! After adding the placeholder, run the following command:
-//!
+//! ## Getting Started
+//! To collect and replace all snippets from multiple data sources, use:
 //! ```sh
 //! snipdoc run
 //! ```
+//! For a detailed guide on how it works, follow this [guid](https://github.com/kaplanelad/snipdoc/tree/main/docs/inject)
+//!  
+//! ### Inject Options
+//! Snipdoc provides several attributes to customize snippet injection:
 //!
-//! #### Inject attributes
-//! Following attributes are available when injecting the snippets as a
-//! attribute.
+//! #### Adding a Prefix to Snippets
+//! The `add_prefix` attribute allows you to prepend a specified string to each
+//! line of the snippet. This is useful when you need to format the injected
+//! snippets with a specific prefix, such as for comments in code blocks.
 //!
-//! ##### `add_prefix`
-//! For adding a prefix for each snippet line use the `add_prefix` attribute.
+//! [Check out this example](https://github.com/kaplanelad/snipdoc/tree/main/docs/add_prefix)
 //!
-//! ##### `strip_prefix`
-//! For removing a prefix for each snippet line use `strip_prefix` attribute
+//! #### Removing a Prefix from Snippets
+//! The `strip_prefix` attribute allows you to remove a specified string from
+//! the beginning of each line in the snippet. This is useful when you need to
+//! format the injected snippets by stripping out comment prefixes or other
+//! unwanted characters.
 //!
-//! ##### `template`
-//! Wrap the snippet content with a custom template use `template` attribute.
+//! [Check out this example](https://github.com/kaplanelad/snipdoc/tree/main/docs/strip_prefix/)
+//!
+//! #### Using Templates
+//! The `template` attribute allows you to wrap your snippet with a given
+//! template. This is useful when you have a snippet that you want to format in
+//! a specific way, such as wrapping it with a YAML tag format.
+//!
+//! [Check out this example](https://github.com/kaplanelad/snipdoc/tree/main/docs/template/)
+//!
+//! #### Executing Snippet Content
+//! The `execute` action option allows you to execute a snippet as a shell
+//! command and collect the output of the command into a snippet. This is useful
+//! when you want to add the result of a `--help` command to your documentation,
+//! ensuring that your documentation stays up-to-date with your CLI tool's
+//! output, even if it changes.
+//!
+//! [Check out this example](https://github.com/kaplanelad/snipdoc/tree/main/docs/execute_snippet_content/)
 //!
 //!
 //! ### Managing Snippets
 //!
-//! To manage all snippets effectively, run the following command:
-//!
+//! To manage all snippets effectively, run:
 //! ```sh
 //! snipdoc show
 //! ```
 //!
-//! For a live example, run the following command inside your project:
-//! ```sh
-//!  snipdoc show ./snipdoc/examples/inject
-//! ```
+//! ### Creating a YAML File
 //!
-//! ### Managing Snippets in YAML
+//! You can mix snippets from your code with a YAML file configuration. Create
+//! an empty snipdoc.yml file by running:
 //!
-//! If you prefer managing snippets in a YAML file, follow these steps:
-//!
-//! 1. Run the following command to create an empty `snipdoc.yml` file: ```sh
-//!    snipdoc create-db --empty ```
-//! 2. Add your snippets to `snipdoc.yml`, and use them by injecting from YAML
-//!    (`inject_from="code"`).
-//! For a live example, run the following command inside your project:
+//! Create an `snipdoc.yml` file by running the command:
 //!    ```sh
-//!    snipdoc run ./snipdoc/examples/inject
+//!    snipdoc create-db --empty
 //!    ```
 //!
-//! ### Check
 //!
-//! validate that all snippets are valid and match the current injected
-//! versions. It is useful for incorporating into CI workflows to ensure
-//! documentation accuracy and consistency.
+//! ### Checking Snippets
+//!
+//! Validate that all snippets are valid and match the current injected
+//! versions. This is useful for CI workflows to ensure documentation accuracy
+//! and consistency.
 //!
 //! ```sh
 //! snipdoc check
 //! ```
+//!
+//! #### Github Action
+//! To integrate Snipdoc with GitHub Actions, use the following workflow
+//! configuration:
+//!
+//!
+//! ```yaml
+//! name: docs
+//!
+//! jobs:
+//!   check:
+//!     name: Check
+//!     runs-on: ubuntu-latest
+//!     permissions:
+//!       contents: read
+//!     steps:
+//!       - name: Checkout the code
+//!         uses: actions/checkout@v4
+//!       - uses: actions-rs/toolchain@v1
+//!         with:
+//!           profile: minimal
+//!           toolchain: stable
+//!           override: true
+//!       - run: cargo install snipdoc        
+//!       - run: snipdoc check        
+//! ```
+
 #[cfg(feature = "cli")]
 pub mod cli;
 pub mod db;
