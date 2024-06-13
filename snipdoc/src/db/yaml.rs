dc14c9832de734abc8f6174aaf80592143c2dba6
Elad Kaplan <kaplan.elad@gmail.com>
2024-06-13 12:20:56 +0300

    read from file

--- snipdoc/src/db/yaml.rs
+++ snipdoc/src/db/yaml.rs
@@ -102,9 +102,10 @@ impl Db for Yaml {
 
 #[cfg(test)]
 mod tests {
-    use insta::assert_debug_snapshot;
+    use insta::{assert_debug_snapshot, with_settings};
 
     use super::*;
+    use crate::tests_cfg;
 
     #[test]
     fn can_load() {
@@ -123,7 +124,7 @@ mod tests {
                     path: ./snipdoc-snippets.yaml
         ";
 
-        let path: PathBuf = tree_fs::from_yaml_str(yaml_content).unwrap();
+        let path = tree_fs::from_yaml_str(yaml_content).unwrap();
         let yaml_db = Yaml::new(path.join("snipdoc-snippets.yaml").as_path());
         assert_debug_snapshot!(yaml_db.load());
     }
@@ -144,7 +145,7 @@ mod tests {
         assert!(Yaml::try_from_default_file(Path::new("path")).is_none());
     }
 
-    #[cfg(windows)]
+    #[cfg(not(windows))]
     #[test]
     fn can_save() {
         let root_folder = tree_fs::Tree::default().root_folder;
