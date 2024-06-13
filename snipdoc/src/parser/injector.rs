b67c0b32f1eb316abb3e2298088914072fb88cdd
Elad Kaplan <kaplan.elad@gmail.com>
2024-06-13 12:39:33 +0300

    inject

--- snipdoc/src/parser/injector.rs
+++ snipdoc/src/parser/injector.rs
@@ -516,10 +516,19 @@ not-found
         let snippet_refs: HashMap<String, &Snippet> =
             snippets.iter().map(|(k, v)| (k.clone(), v)).collect();
 
-        // #[cfg(not(windows))]
-        // let redact = tests_cfg::redact::all();
-        // #[cfg(windows)]
-        let redact = vec![(r"\\n", tests_cfg::redact::REDACT_NEW_LINE)];
+        #[cfg(not(windows))]
+        let redact = tests_cfg::redact::all();
+        #[cfg(windows)]
+        let redact = {
+            let mut redact = vec![
+                (r"\\n\\n", tests_cfg::redact::REDACT_NEW_LINE),
+                (
+                    tests_cfg::redact::REGEX_REPLACE_LINE_ENDING,
+                    tests_cfg::redact::REDACT_NEW_LINE,
+                ),
+            ];
+            redact
+        };
         with_settings!({filters => redact}, {
             assert_debug_snapshot!(injector.run(&snippet_refs));
         });
