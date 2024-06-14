cff584606e15a155823f29061f5db1ef271c3099
Elad Kaplan <kaplan.elad@gmail.com>
2024-06-14 09:50:42 +0300

    windows ci

--- snipdoc/src/config.rs
+++ snipdoc/src/config.rs
@@ -1,7 +1,7 @@
 //! This module provides configuration management for the `snipdoc`. It
 //! includes functionality to load and manage configurations from a default YAML
 //! file.
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use regex::Regex;
 use serde::{Deserialize, Serialize};
@@ -29,9 +29,6 @@ pub struct WalkConfig {
     /// Patterns to exclude files.
     #[serde(with = "serde_regex", default)]
     pub excludes: Vec<Regex>,
-    /// Patterns to exclude files.
-    #[serde(default)]
-    pub excludes_file_path: Vec<PathBuf>,
 }
 
 impl Config {
--- snipdoc/src/walk.rs
+++ snipdoc/src/walk.rs
@@ -89,19 +89,6 @@ impl Walk {
         false
     }
 
-    fn exclude_file_path(&self, path: &Path) -> bool {
-        for exclude_file in &self.config.excludes_file_path {
-            let absolute_path =
-                dunce::canonicalize(exclude_file).unwrap_or_else(|_| exclude_file.clone());
-            if absolute_path == path {
-                tracing::debug!("file should not be included");
-                return true;
-            }
-        }
-
-        false
-    }
-
     /// Collects files in the specified folder, respecting exclude and include
     /// patterns.
     #[must_use]
@@ -119,7 +106,7 @@ impl Walk {
                         |entry| {
                             if entry.path().is_file() {
                                 let path = entry.path().to_owned();
-                                if !self.exclude_file_path(path.as_path()) && !self.should_exclude(path.as_path()) && self.should_include(path.as_path()){
+                                if !self.should_exclude(path.as_path()) && self.should_include(path.as_path()){
                                     if let Err(err) = tx.send(path.clone()) {
                                         tracing::error!(err = %err,path = %path.display(),"error sending path to tx ");
                                     }
@@ -132,5 +119,56 @@ impl Walk {
             });
 
         rx.into_iter().collect::<Vec<_>>()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use insta::assert_debug_snapshot;
+    use regex::Regex;
+
+    use super::*;
+
+    #[test]
+    fn can_exclude_files() {
+        let yaml_content = r"
+        files:
+        - path: README.md
+          content: 
+        - path: file.txt
+          content: 
+        - path: folder/file.txt
+          content: 
+        - path: ignores/ignore.json
+          content: 
+        ";
+
+        let root_path = tree_fs::from_yaml_str(yaml_content).unwrap();
+
+        let config = WalkConfig {
+            includes: vec![],
+            excludes: vec![Regex::new(r"ignores[/|\\]").unwrap()],
+        };
+        let walk = Walk::from_config(&root_path, &config).unwrap();
+
+        let files: Vec<PathBuf> = walk.get_files();
+
+        let mut file_paths: Vec<PathBuf> = files
+            .into_iter()
+            .map(|file_path| {
+                file_path
+                    .strip_prefix(&walk.folder)
+                    .unwrap_or(&file_path)
+                    .to_path_buf()
+            })
+            .collect();
+
+        file_paths.sort_by(|a, b| {
+            a.to_string_lossy()
+                .to_lowercase()
+                .cmp(&b.to_string_lossy().to_lowercase())
+        });
+
+        assert_debug_snapshot!(file_paths);
     }
 }
